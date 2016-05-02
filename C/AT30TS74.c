c6bcc63a989f99b5cb906c137fd036118ca78628
Yadwinder <ryker1990@gmail.com>
2016-05-02 14:14:14 +0530

    first commit

---
+++ C/AT30TS74.c
@@ -0,0 +1,60 @@
+// Distributed with a free-will license.
+// Use it any way you want, profit or free, provided it fits in the licenses of its associated works.
+// AT30TS74
+// This code is designed to work with the AT30TS74_I2CS I2C Mini Module available from ControlEverything.com.
+// https://www.controleverything.com/products
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+
+void main() 
+{
+	// Create I2C bus
+	int file;
+	char *bus = "/dev/i2c-1";
+	if((file = open(bus, O_RDWR)) < 0) 
+	{
+		printf("Failed to open the bus. \n");
+		exit(1);
+	}
+	// Get I2C device, AT30TS74 I2C address is 0x48(72)
+	ioctl(file, I2C_SLAVE, 0x48);
+
+	// Select configuration register(0x01)
+	// Set resolution = 12-bits, normal operations, comparator mode(0x60)
+	// Reserved, default to 0x00
+	char config[3] = {0};
+	config[0] = 0x01;
+	config[1] = 0x60;
+	config[2] = 0x00;
+	write(file, config, 3);
+	sleep(1);
+
+	// Read 2 byte of data from register(0x00)
+	// temp msb, temp lsb
+	char reg[1] = {0x00};
+	write(file, reg, 1);
+	char data[2] = {0};
+	if(read(file, data, 2) != 2)
+	{
+		printf("Error : Input/output Error \n");
+	}
+	else
+	{
+		// Convert the data to 12 bit
+		int temp = (data[0] * 256 + (data[1] & 0xF0)) / 16;
+		if(temp > 2047)
+		{
+			temp -= 4096;
+		}
+		float cTemp = temp * 0.0625;
+		float fTemp = (cTemp * 1.8) + 32;
+
+		// Output data to screen
+		printf("Temperature in Celsius : %.2f C \n", cTemp);
+		printf("Temperature in Fahrenheit : %.2f F \n", fTemp);
+	}
+}
